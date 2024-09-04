8dbeb959265377953264610184e354728cc08211
Jeongho Nam <samchon.github@gmail.com>
2024-09-04 13:47:33 +0900

    Try sitemap again

--- build/deploy.js
+++ build/deploy.js
@@ -1,26 +1,13 @@
 const cp = require("child_process");
 const deploy = require("gh-pages");
-const fs = require("fs");
-const path = require("path");
 
 cp.execSync("npm run build", {
   stdio: "inherit",
   cwd: `${__dirname}/..`,
 });
 
-const directory = fs.readdirSync(`${__dirname}/../public`);
-for (const file of directory)
-  if (
-    file === "robots.txt" ||
-    (file.startsWith("sitemap") && file.endsWith(".xml"))
-  )
-    fs.copyFileSync(
-      `${__dirname}/../public/${file}`,
-      `${__dirname}/../out/${file}`
-    );
-
 deploy.publish(
-  path.resolve(`${__dirname}/../out`),
+  `${__dirname}/../out`,
   {
     branch: "gh-pages",
     dotfiles: true,
