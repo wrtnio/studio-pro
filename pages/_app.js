880ebaacad6e3958db7f56eb094a731cbd35091f
Jeongho Nam <samchon.github@gmail.com>
2024-09-20 22:39:35 +0900

    Add clarity

--- pages/_app.js
+++ pages/_app.js
@@ -10,10 +10,10 @@ export default function Nextra({ Component, pageProps }) {
         dangerouslySetInnerHTML={{
           __html: `
 (function(c,l,a,r,i,t,y){
-    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "gu5qz1srj3");
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "o622b478i3");
                     `,
         }}
       />
